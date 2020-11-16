c6a8632508b3a93d25b23ec7706e9d9f524b521e
19021362 <62606120+19021362@users.noreply.github.com>
2020-11-16 21:45:29 +0700

    Update Entity.java

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -1,12 +1,10 @@
 package uet.oop.bomberman.entities;
 
 
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.loadMap;
@@ -19,6 +17,8 @@ public abstract class Entity {
     protected int y;
 
     protected Image img;
+
+    protected int status = 0;
 
     //Khởi tạo đối tượng, chuyển từ tọa độ đơn vị sang tọa độ trong canvas
     public Entity( int xUnit, int yUnit, Image img) {
@@ -35,5 +35,15 @@ public abstract class Entity {
 
     public void move(Scene scene, loadMap map) {}
 
+    protected void remove(Entity o) {
+        this.img = Sprite.grass.getFxImage();
+    }
 
+    public Rectangle getBound() {
+        return new Rectangle(this.x + 2, this.y + 2, Sprite.SCALED_SIZE - 2, Sprite.SCALED_SIZE -2);
+    }
+
+    public boolean collision(Entity o) {
+        return this.getBound().intersects(o.getBound().getBoundsInParent());
+    }
 }
