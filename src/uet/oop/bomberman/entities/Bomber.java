434812fe5333a67782db2f5eab6098ff652d7bb2
hmquang01 <hminhquang251101@gmail.com>
2020-11-17 15:14:29 +0700

    Cập nhật hẳn hoi thử 1 lần

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -9,18 +9,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import uet.oop.bomberman.BombermanGame;
 import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.loadMap;
 
 import java.util.List;
 
 public class Bomber extends Entity {
-
-    private int speed = 4;
-                                         // Lấy Status % 3 để luân phiên lấy 3 giá trị 0 1 2, mỗi giá trị ứng với một tư thế chạy (1 ảnh)
-    private int side = 1;                // Hướng chạy hiện tại. 1: Trái -> Phải. -1: Phải -> Trái
-    //private int status = -1;              // Lấy Status % 3 để luân phiên lấy 3 giá trị 0 1 2, mỗi giá trị ứng với một tư thế chạy (1 ảnh)
-
+    private int speed = 8;
     private int side_h = 1;              // Hướng chạy hiện tại. 1: Trái -> Phải. -1: Phải -> Trái
     private int side_v = 1;              // Hướng chạy hiện tại. 1: Trên -> Dưới. -1: Dưới -> Trên
 
@@ -31,6 +27,119 @@ public class Bomber extends Entity {
 
     @Override
     public void update() {
+        calculateDir();
+    }
+
+    /**
+     * Bắt xự kiện từ bàn phím
+     * @param scene màn hình.
+     * @param map bản đồ để khi ấn SPACE thì đặt bom.
+     */
+    @Override
+    public void move(Scene scene, loadMap map) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                KeyCode keyCode = event.getCode();
+                switch (keyCode) {
+                    case RIGHT:
+                        side_v = 0;
+                        if(side_h == 1){               //Không chuyển hướng: Giữ trạng thái
+                            status++;
+                        }
+                        else{                          //Chuyển hướng: Đặt về trạng thái, hướng vừa ấn
+                            side_h = 1;
+                            status = 0;
+                        }
+                        if((x+24) % 32 == 0 ){
+                            if(canPass(1,0, map)){
+                                x += speed;
+                            }
+                            else{
+                                System.out.println("cant pass");
+                            }
+                        }
+                        else{
+                            x += speed;
+                        }
+                        System.out.println(x +" " + y);
+                        break;
+                    case LEFT:
+                        side_v = 0;
+                        if(side_h == -1){
+                            status++;
+                        }
+                        else{
+                            side_h = -1;
+                            status = 0;
+                        }
+                        if(x % 32 == 0 ){
+                            if(canPass(-1,0, map)){
+                                x -= speed;
+                            }
+                            else{
+                                System.out.println("cant pass");
+                            }
+                        }
+                        else{
+                            x -= speed;
+                        }
+                        System.out.println(x +" " + y);
+                        break;
+                    case UP:
+                        side_h = 0;
+                        if(side_v == -1){ status++;}
+                        else{
+                            side_v = -1;
+                            status = 0;
+                        }
+                        if(y % 32 == 0 ){
+                            if(canPass(0,-1, map)){
+                                y -= speed;
+                            }
+                            else{
+                                System.out.println("cant pass");
+                            }
+                        }
+                        else{
+                            y -= speed;
+                        }
+                        System.out.println(x +" " + y);
+                        break;
+                    case DOWN:
+                        side_h = 0;
+                        if(side_v == 1){
+                            status++;
+                        }
+                        else{
+                            side_v = 1;
+                            status = 0;
+                        }
+                        System.out.println(x +" " + y);
+                        if(y % 32 == 0 ){
+                            if(canPass(0,1, map)){
+                                y += speed;
+                            }
+                            else{
+                                System.out.println("cant pass");
+                            }
+                        }
+                        else{
+                            y += speed;
+                        }
+                        break;
+                    case SPACE:
+                        Entity bomb = new Bomb((x + Sprite.DEFAULT_SIZE) / Sprite.SCALED_SIZE,
+                                (y + Sprite.DEFAULT_SIZE) / Sprite.SCALED_SIZE,
+                                Sprite.bomb.getFxImage(), map);
+                        map.add(bomb);
+                        break;
+                }
+            }
+        });
+    }
+
+    void calculateDir(){
         if(side_h == 1) {                       //Nếu đang chạy trái sang phải thì dùng 3 ảnh dưới
             if (status % 9 == 0) {              //%9 thay vì %3 và status == 0 3 6 để giảm tốc độ cử động, muốn chậm hơn có thể % 27, ...( vẩy tay vẩy chân quá nhanh trong khi di chuyển chậm, nên sửa test status % 3 == 0, 1, 2 để hiểu hơn)
                 this.img = Sprite.player_right.getFxImage();
@@ -71,108 +180,68 @@ public class Bomber extends Entity {
     }
 
     /**
-     * Bắt xự kiện từ bàn phím
-     * @param scene màn hình.
-     * @param map bản đồ để khi ấn SPACE thì đặt bom.
+     * Cái này dùng tọa độ hơi phức tạp 1 chút.
+     * -Chiều ngang : nếu bomber đang đứng khít trong 1 ô, xét xem ô tiếp theo hoặc trước đó
+     * (tùy theo phím), nếu là grass thì cho đi tiếp. Nếu bomber đứng ở giữa 2 ô thì phải xét 2 ô tiếp
+     * -Chiều dọc : cũng tương tự như chiều ngang nhưng vì bomber rộng 24 nên nó đi up down được ở 2 vị trí
+     * @param h horizontal~ theo chiều ngang
+     * @param v vertical~ theo chiều dọc
+     * @param map bản đồ
+     * @return
      */
-    @Override
-    public void move(Scene scene, loadMap map) {
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                    KeyCode keyCode = event.getCode();
-                    switch (keyCode) {
-                        case RIGHT:
-                            side_v = 0;
-                            if (side_h == 1) {               //Không chuyển hướng: Giữ trạng thái
-                                status++;
-                            } else {                          //Chuyển hướng: Đặt về trạng thái, hướng vừa ấn
-                                side_h = 1;
-                                status = 0;
-                            }
-                            x += speed;
-                            if (collision(map)) {
-                                x -= speed;
-                            }
-                            break;
-                        case LEFT:
-                            side_v = 0;
-                            if (side_h == -1) {
-                                status++;
-                            } else {
-                                side_h = -1;
-                                status = 0;
-                            }
-                            x -= speed;
-                            if (collision(map)) {
-                                x += speed;
-                            }
-                            break;
-                        case UP:
-                            side_h = 0;
-                            if (side_v == -1) {
-                                status++;
-                            } else {
-                                side_v = -1;
-                                status = 0;
-                            }
-                            y -= speed;
-                            if (collision(map)) {
-                                y += speed;
-                            }
-                            break;
-                        case DOWN:
-                            side_h = 0;
-                            if (side_v == 1) {
-                                status++;
-                            } else {
-                                side_v = 1;
-                                status = 0;
-                            }
-                            y += speed;
-                            if (collision(map)) {
-                                y -= speed;
-                            }
-                            break;
-                        case SPACE:
-                            Entity bomb = new Bomb((x + Sprite.DEFAULT_SIZE) / Sprite.SCALED_SIZE,
-                                    (y + Sprite.DEFAULT_SIZE) / Sprite.SCALED_SIZE,
-                                    Sprite.bomb.getFxImage());
-                            map.add(bomb);
-                            break;
-                        default:
-                            check(map);
-                    }
+    boolean canPass(int h, int v, loadMap map){
+        if(h==1){
+            if(y%32==0){
+                Entity e = map.getStillObjects().get((int)(x/32) + 1 + (y/32)* BombermanGame.WIDTH);
+                if(e instanceof Grass)                          {return true; }
+                else                                            {return false;}
             }
-        });
-    }
-
-    /**
-     * check không cho vật thể đi ra ngoài màn hình.
-     */
-    private void check(loadMap map) {
-        if (collision(map)) {
-            speed = 0;
-        }
-    }
-
-    private boolean collision(loadMap map) {
-        boolean check = false;
-        List<Entity> obj = map.getStillObjects();
-        for (Entity o : obj) {
-            if (o instanceof Wall && this.collision(o)) {
-                check = true;
-                break;
-            }
-            if (o instanceof Brick && this.collision(o)) {
-                check = true;
-                break;
-            }
-            if (o instanceof Portal && this.collision(o)) {
-                check = true;
-                break;
+            else{
+                Entity e1 = map.getStillObjects().get((int)(x/32) + 1 + (y/32)* BombermanGame.WIDTH);
+                Entity e2 = map.getStillObjects().get((int)(x/32) + 1 + (y/32+1)* BombermanGame.WIDTH);
+                if(e1 instanceof Grass && e2 instanceof Grass)  {return true; }
+                else                                            {return false;}
             }
         }
-        return check;
+        if(h==-1){
+            if(y%32==0){
+                Entity e = map.getStillObjects().get((int)(x/32) - 1 + (y/32)* BombermanGame.WIDTH);
+                if(e instanceof Grass)                          {return true; }
+                else                                            {return false;}
+            }
+            else{
+                Entity e1 = map.getStillObjects().get((int)(x/32) -1 + (y/32)* BombermanGame.WIDTH);
+                Entity e2 = map.getStillObjects().get((int)(x/32) - 1 + (y/32+1)* BombermanGame.WIDTH);
+                if(e1 instanceof Grass && e2 instanceof Grass)  {return true; }
+                else                                            {return false;}
+            }
+        }
+        if(v==-1){
+            if(x%32==0 || x%32==8){
+                Entity e = map.getStillObjects().get((int)(x/32) + (y/32-1)* BombermanGame.WIDTH);
+                if(e instanceof Grass)                          {return true; }
+                else                                             {return false;}
+            }
+            else{
+                Entity e1 = map.getStillObjects().get((int)(x/32) + 1 + (y/32-1)* BombermanGame.WIDTH);
+                Entity e2 = map.getStillObjects().get((int)(x/32)  + (y/32-1)* BombermanGame.WIDTH);
+                if(e1 instanceof Grass && e2 instanceof Grass)  {return true; }
+                else                                            {return false;}
+            }
+        }
+        if(v==1){
+            if(x%32==0 || x%32==8){
+                Entity e = map.getStillObjects().get((int)(x/32) + (y/32+1)* BombermanGame.WIDTH);
+                if(e instanceof Grass)                          {return true; }
+                else                                             {return false;}
+            }
+            else{
+                Entity e1 = map.getStillObjects().get((int)(x/32) + 1 + (y/32+1)* BombermanGame.WIDTH);
+                Entity e2 = map.getStillObjects().get((int)(x/32)  + (y/32+1)* BombermanGame.WIDTH);
+                if(e1 instanceof Grass && e2 instanceof Grass)  {return true; }
+                else                                            {return false;}
+            }
+        }
+        return false;
     }
 }
