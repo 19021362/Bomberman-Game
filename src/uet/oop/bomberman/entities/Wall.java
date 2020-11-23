2b36641efccc00591301d5add8d361f3f62fbbaa
19021362 <62606120+19021362@users.noreply.github.com>
2020-11-23 08:08:31 +0700

    Update Wall.java

--- src/uet/oop/bomberman/entities/Wall.java
+++ src/uet/oop/bomberman/entities/Wall.java
@@ -7,10 +7,12 @@ public class Wall extends Entity {
 
     public Wall(int x, int y, Image img) {
         super(x, y, img);
+        setCanPass(false);
     }
 
     @Override
     public void update() {
 
     }
+
 }
