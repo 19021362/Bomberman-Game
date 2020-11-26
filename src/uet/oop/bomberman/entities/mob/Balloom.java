2c77f799a448bc89366aee930b6e7ae44e2b81df
19021362 <62606120+19021362@users.noreply.github.com>
2020-11-26 14:40:41 +0700

    Create Balloom.java

---
+++ src/uet/oop/bomberman/entities/mob/Balloom.java
@@ -0,0 +1,113 @@
+package uet.oop.bomberman.entities.mob;
+
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import uet.oop.bomberman.BombermanGame;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.explosion.Bomb;
+import uet.oop.bomberman.entities.explosion.Explosion;
+import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.loadMap;
+
+import java.util.List;
+import java.util.Random;
+
+public class Balloom extends Mob {
+    private int speed = 1;
+    private int dir = 0;
+    private int side_h = 1;     // Hướng chạy hiện tại. 1: Trái -> Phải. -1: Phải -> Trái.
+    private Random rd = new Random();
+    public Balloom(int x, int y, Image img) {
+        super( x, y, img);
+    }
+
+    @Override
+    public void update() {
+        if (live) {
+            calculateDir();
+            move();
+        } else {
+            if (animation == 30) {
+                remove(this);
+            } else {
+                animation++;
+                kill();
+            }
+        }
+    }
+
+
+    void calculateDir(){
+        if(side_h == 1) {                       //Nếu đang chạy trái sang phải thì dùng 3 ảnh dưới
+            if (status % 9 == 0) {              //%9 thay vì %3 và status == 0 3 6 để giảm tốc độ cử động, muốn chậm hơn có thể % 27, ...( vẩy tay vẩy chân quá nhanh trong khi di chuyển chậm, nên sửa test status % 3 == 0, 1, 2 để hiểu hơn)
+                this.img = Sprite.balloom_right1.getFxImage();
+            } else if (status % 9 == 3) {
+                this.img = Sprite.balloom_right2.getFxImage();
+            } else if (status % 9 == 6){
+                this.img = Sprite.balloom_right3.getFxImage();
+            }
+        }
+        if(side_h == -1) {                       //Nếu đang chạy trái sang phải thì dùng 3 ảnh dưới
+            if (status % 9 == 0) {              //%9 thay vì %3 và status == 0 3 6 để giảm tốc độ cử động, muốn chậm hơn có thể % 27, ...( vẩy tay vẩy chân quá nhanh trong khi di chuyển chậm, nên sửa test status % 3 == 0, 1, 2 để hiểu hơn)
+                this.img = Sprite.balloom_left1.getFxImage();
+            } else if (status % 9 == 3) {
+                this.img = Sprite.balloom_left2.getFxImage();
+            } else if (status % 9 == 6){
+                this.img = Sprite.balloom_left3.getFxImage();
+            }
+        }
+    }
+
+    public void move() {
+        switch (dir){
+            case 0:
+                if(side_h == 1) {status++;}
+                else{
+                    side_h = 1;
+                    status = 0;
+                }
+                x += speed;
+                if (collision()) {
+                    x -= speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                    break;
+            case 1:
+                status++;
+                y += speed;
+                if (collision()) {
+                    y -= speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                break;
+            case 2:
+                if(side_h == -1) {status++;}
+                else{
+                    side_h = -1;
+                    status = 0;
+                }
+                x -= speed;
+                if (collision()) {
+                    x += speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                break;
+            case 3:
+                status++;
+                y -= speed;
+                if (collision()) {
+                    y += speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void kill() {
+        img = Sprite.movingSprite(Sprite.mob_dead1, Sprite.mob_dead2, Sprite.mob_dead3, animation, 30).getFxImage();
+    }
+}
