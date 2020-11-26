7cbc8a675e3c8830fd34006024d031e49dafa425
19021362 <62606120+19021362@users.noreply.github.com>
2020-11-26 14:40:46 +0700

    Create Mob.java

---
+++ src/uet/oop/bomberman/entities/mob/Mob.java
@@ -0,0 +1,46 @@
+package uet.oop.bomberman.entities.mob;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.explosion.Explosion;
+import uet.oop.bomberman.loadMap;
+
+import java.util.List;
+
+public abstract class Mob extends Entity {
+    protected int animation = 0;
+    protected boolean live = true;
+
+    public Mob(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    public void move(Scene scene) {}
+
+    @Override
+    public boolean collision() {
+        boolean check = false;
+        for (Entity e : loadMap.getMob()) {
+            if (e instanceof Bomber && this.collision(e)) {
+                ((Bomber) e).setLive(false);
+            }
+        }
+
+        for (Entity o : loadMap.getStillObjects()) {
+            if (!o.canPass && this.collision(o)) {
+                check = true;
+                break;
+            }
+        }
+        return check;
+    }
+
+    public void kill() {
+
+    }
+
+    public void setLive(boolean live) {
+        this.live = live;
+    }
+}
