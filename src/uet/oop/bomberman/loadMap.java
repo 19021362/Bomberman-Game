64f6c9cc55abd9fa02917c6b9b8078e3538c81d1
19021362 <62606120+19021362@users.noreply.github.com>
2020-11-28 23:53:24 +0700

    Update loadMap.java

--- src/uet/oop/bomberman/loadMap.java
+++ src/uet/oop/bomberman/loadMap.java
@@ -2,36 +2,45 @@ package uet.oop.bomberman;
 
 import uet.oop.bomberman.entities.*;
 import uet.oop.bomberman.entities.mob.Balloom;
+import uet.oop.bomberman.entities.mob.Bomber;
 import uet.oop.bomberman.entities.tile.Brick;
 import uet.oop.bomberman.entities.tile.Grass;
 import uet.oop.bomberman.entities.tile.Portal;
 import uet.oop.bomberman.entities.tile.Wall;
+import uet.oop.bomberman.entities.tile.item.Bomb_item;
+import uet.oop.bomberman.entities.tile.item.Live_item;
+import uet.oop.bomberman.entities.tile.item.Speed_item;
 import uet.oop.bomberman.graphics.Sprite;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class loadMap {
-    private String input_map = "";
+    private static String input_level1 = "C:\\Users\\ASUS\\Documents\\GitHub\\Bomberman-Game\\res\\levels\\1.txt";
+    private static String input_Level2 = "C:\\Users\\ASUS\\Documents\\GitHub\\Bomberman-Game\\res\\levels\\2.txt";
+    private static String input = "";
     private static List<Entity> stillObjects = new ArrayList<>();
     private static List<Entity> mob = new ArrayList<>();
+
 
 
     /**
      * ĐỌc bản đồ từ file.
      */
-    public static void load() {
+    public static void load(int level) {
+        Random rd = new Random();
         try {
-            FileReader input =
-                    new FileReader("C:\\Users\\ASUS\\Documents\\GitHub\\Bomberman-Game\\res\\levels\\1.txt");
+            FileReader input = new FileReader(getInput(level));
             Scanner scanner = new Scanner(input);
             int j = 0;
             while (scanner.hasNextLine()) {
                 String s = scanner.nextLine();
                 for (int  i = 0; i < s.length(); i++) {
+                    int k = Math.abs(rd.nextInt() % 3);
                     Entity object;
                     if (s.charAt(i) == '#') {
                         object = new Wall(i, j, Sprite.wall.getFxImage());
@@ -43,6 +52,21 @@ public class loadMap {
                     } else if (s.charAt(i) == 'b') {
                         object = new Grass(i, j, Sprite.grass.getFxImage());
                         mob.add(new Balloom(i, j, Sprite.balloom_left1.getFxImage()));
+                    } else if (s.charAt(i) == 'i') {
+                        stillObjects.add(new Grass(i, j, Sprite.grass.getFxImage()));
+                        if (k == 0) {
+                            stillObjects.add(new Bomb_item(i, j, Sprite.powerup_bombs.getFxImage()));
+                        }
+                        if (k == 1) {
+                            stillObjects.add(new Speed_item(i, j, Sprite.powerup_speed.getFxImage()));
+                        }
+                        if (k == 2) {
+                            stillObjects.add(new Live_item(i, j, Sprite.powerup_bombpass.getFxImage()));
+                        }
+                        object = new Brick(i, j, Sprite.brick.getFxImage());
+                    } else if (s.charAt(i) == 'p') {
+                        object = new Grass(i, j, Sprite.grass.getFxImage());
+                        mob.add(new Bomber(i, j, Sprite.player_right_2.getFxImage()));
                     } else {
                         object = new Grass(i, j, Sprite.grass.getFxImage());
                     }
@@ -72,4 +96,13 @@ public class loadMap {
     public static void remove(Entity o) {
         stillObjects.remove(o);
     }
+
+    private static String getInput(int level) {
+        if (level == 1) {
+            return input_level1;
+        } else {
+            return input_Level2;
+        }
+    }
+
 }
