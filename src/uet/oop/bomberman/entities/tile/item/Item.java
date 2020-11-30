3839fc7e76e325c883a5493b1eb1284d16d64b9b
19021362 <62606120+19021362@users.noreply.github.com>
2020-11-30 20:57:55 +0700

    Update Item.java

--- src/uet/oop/bomberman/entities/tile/item/Item.java
+++ src/uet/oop/bomberman/entities/tile/item/Item.java
@@ -3,11 +3,11 @@ package uet.oop.bomberman.entities.tile.item;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.mob.Bomber;
+import uet.oop.bomberman.graphics.Sound;
 import uet.oop.bomberman.loadMap;
 
 public abstract class Item extends Entity {
 
-    private String power_fx = "C:\\Users\\ASUS\\Documents\\GitHub\\Bomberman-Game\\res\\sounds\\item.WAV";
 
     public Item(int xUnit, int yUnit, Image img) {
         super(xUnit, yUnit, img);
@@ -24,11 +24,13 @@ public abstract class Item extends Entity {
     @Override
     public boolean collision() {
         boolean check = false;
-        for (Entity o : loadMap.getMob()) {
-            if (o instanceof Bomber && this.collision(o)) {
-                check = true;
-                PlayMusic(power_fx);
-                break;
+        if (isActive()) {
+            for (Entity o : loadMap.getMob()) {
+                if (o instanceof Bomber && this.collision(o)) {
+                    check = true;
+                    PlayMusic(Sound.power_fx);
+                    break;
+                }
             }
         }
         return check;
@@ -36,6 +38,15 @@ public abstract class Item extends Entity {
 
     public void power() {
 
+    }
+
+    private boolean isActive() {
+        boolean active = false;
+        int i = loadMap.getStillObjects().indexOf(this) + 1;
+        if (loadMap.getStillObjects().get(i) == null) {
+            active = true;
+        }
+        return active;
     }
 
 
