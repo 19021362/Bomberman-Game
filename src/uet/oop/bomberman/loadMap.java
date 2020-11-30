ac318a10647b5a6a83e10dc034cd91333ed2becb
19021362 <62606120+19021362@users.noreply.github.com>
2020-11-30 20:58:08 +0700

    Update loadMap.java

--- src/uet/oop/bomberman/loadMap.java
+++ src/uet/oop/bomberman/loadMap.java
@@ -22,7 +22,9 @@ import java.util.Scanner;
 public class loadMap {
     private static String input_level1 = "C:\\Users\\ASUS\\Documents\\GitHub\\Bomberman-Game\\res\\levels\\1.txt";
     private static String input_Level2 = "C:\\Users\\ASUS\\Documents\\GitHub\\Bomberman-Game\\res\\levels\\2.txt";
+    private static String input_Level3 = "C:\\Users\\ASUS\\Documents\\GitHub\\Bomberman-Game\\res\\levels\\3.txt";
     private static String input = "";
+    private static boolean nextLevel = false;
     private static List<Entity> stillObjects = new ArrayList<>();
     private static List<Entity> mob = new ArrayList<>();
 
@@ -40,20 +42,11 @@ public class loadMap {
             while (scanner.hasNextLine()) {
                 String s = scanner.nextLine();
                 for (int  i = 0; i < s.length(); i++) {
-                    int k = Math.abs(rd.nextInt() % 3);
+                    int k = Math.abs(rd.nextInt() % 15);
                     Entity object;
                     if (s.charAt(i) == '#') {
                         object = new Wall(i, j, Sprite.wall.getFxImage());
                     } else if (s.charAt(i) == '*') {
-                        stillObjects.add(new Grass(i, j, Sprite.grass.getFxImage()));
-                        object = new Brick(i, j, Sprite.brick.getFxImage());
-                    } else if (s.charAt(i) == 'x') {
-                        object = new Portal(i, j, Sprite.grass.getFxImage());
-                    } else if (s.charAt(i) == 'b') {
-                        object = new Grass(i, j, Sprite.grass.getFxImage());
-                        mob.add(new Balloom(i, j, Sprite.balloom_left1.getFxImage()));
-                        numEnemy++;
-                    } else if (s.charAt(i) == 'i') {
                         stillObjects.add(new Grass(i, j, Sprite.grass.getFxImage()));
                         if (k == 0) {
                             stillObjects.add(new Bomb_item(i, j, Sprite.powerup_bombs.getFxImage()));
@@ -65,6 +58,12 @@ public class loadMap {
                             stillObjects.add(new Live_item(i, j, Sprite.powerup_bombpass.getFxImage()));
                         }
                         object = new Brick(i, j, Sprite.brick.getFxImage());
+                    } else if (s.charAt(i) == 'x') {
+                        object = new Portal(i, j, Sprite.grass.getFxImage());
+                    } else if (s.charAt(i) == 'b') {
+                        object = new Grass(i, j, Sprite.grass.getFxImage());
+                        mob.add(new Balloom(i, j, Sprite.balloom_left1.getFxImage()));
+                        numEnemy++;
                     } else if (s.charAt(i) == 'p') {
                         object = new Grass(i, j, Sprite.grass.getFxImage());
                         mob.add(new Bomber(i, j, Sprite.player_right_2.getFxImage()));
@@ -90,9 +89,6 @@ public class loadMap {
         return mob;
     }
 
-    public static void add(Entity o) {
-        stillObjects.add(o);
-    }
 
     public static void remove(Entity o) {
         stillObjects.remove(o);
@@ -104,7 +100,7 @@ public class loadMap {
         } else if (level == 2) {
             return input_Level2;
         } else {
-            return input_level1;
+            return input_Level3;
         }
     }
 
@@ -137,5 +133,13 @@ public class loadMap {
             }
         }
         return check;
+    }
+
+    public static boolean isNextLevel() {
+        return nextLevel;
+    }
+
+    public static void setNextLevel(boolean nextLevel) {
+        loadMap.nextLevel = nextLevel;
     }
 }
