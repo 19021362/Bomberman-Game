6b0e2650aa4d8c7289c7cc2a7a813ba3537139fd
19021362 <62606120+19021362@users.noreply.github.com>
2020-12-11 10:27:59 +0700

    Update Mob.java

--- src/uet/oop/bomberman/entities/mob/Mob.java
+++ src/uet/oop/bomberman/entities/mob/Mob.java
@@ -4,20 +4,72 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.explosion.Explosion;
+import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.loadMap;
 
 import java.util.List;
+import java.util.Random;
 
 public abstract class Mob extends Entity {
     protected int animation = 0;
     protected int blood = 1;
     protected boolean live = true;
 
+    protected int speed = 2;
+    protected int dir = 0;
+    protected int side_h = 1;     // Hướng chạy hiện tại. 1: Trái -> Phải. -1: Phải -> Trái.
+    protected final Random rd = new Random();
+
     public Mob(int xUnit, int yUnit, Image img) {
         super(xUnit, yUnit, img);
     }
 
     public void move(Scene scene) {}
+
+    protected void randomMove() {
+        switch (dir){
+            case 0:
+                if(side_h == 1) {status++;}
+                else{
+                    side_h = 1;
+                    status = 0;
+                }
+                x += speed;
+                if (collision()) {
+                    x -= speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                break;
+            case 1:
+                status++;
+                y += speed;
+                if (collision()) {
+                    y -= speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                break;
+            case 2:
+                if(side_h == -1) {status++;}
+                else{
+                    side_h = -1;
+                    status = 0;
+                }
+                x -= speed;
+                if (collision()) {
+                    x += speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                break;
+            case 3:
+                status++;
+                y -= speed;
+                if (collision()) {
+                    y += speed;
+                    dir = Math.abs(rd.nextInt() % 4);
+                }
+                break;
+        }
+    }
 
     @Override
     public boolean collision() {
